540f2d8a7ff731108c1a94e81719ace5da68695f
Umit Oner <umit.oner@weareadaptive.com>
2021-09-23 17:31:29 +0200

    timer functionality initialized within state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState, useEffect } from "react";
+
+const convertTimeToString = (time) => {
+  const seconds = Math.floor((time / 1000) % 60);
+  const minutes = Math.floor((time / (1000 * 60)) % 60);
+  const hundredths = Math.floor((time / 10) % 100);
+
+  const padNumber = (number) => number.toString().padStart(2, "0");
+  return `${padNumber(minutes)}:${padNumber(seconds)}.${padNumber(hundredths)}`;
+};
+
+const stopTimer = (timer) => timer.pauseTime - timer.startTime;
 
 function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const [timer, setTimer] = useState({
+    startTime: 0,
+    pauseTime: 0,
+    totalTime: 0,
+    currentLapTime: 0,
+    lapTimes: [],
+  });
+
+  const timeElapsed = (startTime) => {
+    return Date.now() - startTime;
+  };
+
+  const start = () => {
+    setIsRunning(true);
+    setTimer({ ...timer, startTime: Date.now() });
+  };
+
+  const stop = () => {
+    setIsRunning(false);
+    setIsPaused(true);
+    setTimer((timer) => {
+      return { ...timer, pauseTime: Date.now() };
+    });
+  };
+
+  useEffect(() => {
+    if (isRunning) {
+      const timerID = setInterval(() => {
+        setTimer({ ...timer, totalTime: timeElapsed(timer.startTime) });
+      }, 100);
+
+      return () => {
+        clearInterval(timerID);
+      };
+    }
+  }, [isRunning, timer.totalTime]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top">
+        <div className="digits">
+          <p>{convertTimeToString(timeElapsed(timer.startTime))}</p>
+        </div>
+        <div className="buttons">
+          <button className="start" onClick={start}>
+            Start
+          </button>
+          <button className="stop" onClick={stop}>
+            Stop
+          </button>
+        </div>
+      </div>
+      <div className="bottom">
+        <table>
+          <tbody></tbody>
+        </table>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// useEffect(() => {
+//   const interval = setInterval(() => {
+//     console.log("This will run every second!");
+//   }, 1000);
+//   return () => clearInterval(interval);
+// }, []);
+
+// function handleTimer(startTimeStamp, elapsedTimeStamp) {
+//   if (!isRunning) {
+//     timer = Math.floor(Date.now() - startTimeStamp);
+//     setInterval(convertTimeToString(timer), 10);
+//   }
+
+//   return convertTimeToString(timer);
+// }
+
+//Laps array
+// Ms to Minutes Seconds Milliseconds
+//<tr key={lapID}><td>Lap {lapID}</td><td>{lapTime}</td></tr>
+//getMs(lapTime)
